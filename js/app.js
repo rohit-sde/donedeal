48f9fd7a7d9a5873b4b2458bdabc6331fda23250
Rohit Singh <rohitsinghpb4@gmail.com>
2023-11-30 19:49:30 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -8,20 +8,77 @@ document.addEventListener("DOMContentLoaded", function () {
     let year_long = document.querySelector(".year");
     let time_long = document.querySelector(".full_time");
 
+    // function add_item(e) {
+    //     e.preventDefault();
+    //     let new_value = document.getElementById("new").value;
+    //     let li = document.createElement("li");
+    //     li.className = "list_items";
+    //     li.appendChild(document.createTextNode(new_value));
+    //     items_list.appendChild(li);
+
+    //     let delete_button = document.createElement("button");
+    //     delete_button.className = "delete";
+    //     delete_button.appendChild(document.createTextNode("remove"));
+    //     li.appendChild(delete_button);
+    //     document.querySelector("#new").value = "";
+
+
+    //     if (items_list.children.length == 0) {
+    //         items_list.classList.add("bg");
+    //     } else {
+    //         items_list.classList.remove("bg");
+    //     }
+    // }
     function add_item(e) {
         e.preventDefault();
         let new_value = document.getElementById("new").value;
         let li = document.createElement("li");
         li.className = "list_items";
-        li.appendChild(document.createTextNode(new_value));
+        let new_span_element = document.createElement("span");
+        new_span_element.className = "idea_text";
+
+        let time_create = new Date();
+        let day_create = time_create.toLocaleString('default', { weekday: 'short' });
+        let date_create = time_create.toLocaleString('default', { day: '2-digit' });
+        let hour = time_create.getHours();
+        let minutes = time_create.getMinutes();
+        
+        let anti_post_meridian = "am";
+
+        if (hour > 12 && hour < 24) {
+            hour = hour - 12;
+            anti_post_meridian = "pm";
+        }
+        if (hour == 0) {
+            hour = 12 - hour;
+            anti_post_meridian = "am";
+        }
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+        }
+        new_span_element.setAttribute("data-idea", `Created on ${day_create} ${date_create} at ${hour}:${minutes} ${anti_post_meridian}`);
+
+        let timestamp_record;
+        if (localStorage.getItem('timestamp_record') == null) {
+            timestamp_record = [];
+        }
+        else {
+            timestamp_record = JSON.parse(localStorage.getItem('timestamp_record'));
+        }
+        timestamp_record.push(new_span_element.getAttribute("data-idea"));
+        localStorage.setItem('timestamp_record', JSON.stringify(timestamp_record));
+
+        new_span_element.appendChild(document.createTextNode(new_value));
+        li.appendChild(new_span_element);
         items_list.appendChild(li);
 
         let delete_button = document.createElement("button");
         delete_button.className = "delete";
         delete_button.appendChild(document.createTextNode("remove"));
         li.appendChild(delete_button);
-        document.querySelector("#new").value = "";
 
+        storage(new_value);
+        document.querySelector("#new").value = "";
 
         if (items_list.children.length == 0) {
             items_list.classList.add("bg");
@@ -33,6 +90,23 @@ document.addEventListener("DOMContentLoaded", function () {
         if (e.target.classList.contains("delete")) {
             if (confirm("Are you sure you want to delete this?")) {
                 let li = e.target.parentElement;
+                let li_content = li.firstChild.innerText;
+                let li_span_attr = li.firstChild.getAttribute("data-idea");
+                let ideas = JSON.parse(localStorage.getItem('ideas'));
+                let timestamp_record = JSON.parse(localStorage.getItem('timestamp_record'));
+                
+                ideas.forEach((idea, index) => {
+                    if(idea == li_content) {
+                        ideas.splice(index, 1);
+                    }
+                    localStorage.setItem('ideas', JSON.stringify(ideas));
+                })
+                timestamp_record.forEach((timestamp, index) => {
+                    if(timestamp == li_span_attr) {
+                        timestamp_record.splice(index, 1);
+                    }
+                    localStorage.setItem('timestamp_record', JSON.stringify(timestamp_record));
+                })
                 items_list.removeChild(li);
             }
             if (items_list.children.length == 0) {
@@ -82,6 +156,60 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             items_list.classList.remove("bg");
         }
+    }
+    function storage(idea) {
+        let ideas;
+        if (localStorage.getItem('ideas') == null) {
+            ideas = [];
+        }
+        else {
+            ideas = JSON.parse(localStorage.getItem('ideas'));
+        }
+        ideas.push(idea);
+        localStorage.setItem('ideas', JSON.stringify(ideas));
+    }
+    function update_ui_from_storage() {
+        let ideas;
+        if (localStorage.getItem('ideas') == null) {
+            ideas = [];
+        }
+        else {
+            ideas = JSON.parse(localStorage.getItem('ideas'));
+        }
+        ideas.forEach((idea, index) => {
+            let li = document.createElement("li");
+            li.className = "list_items";
+            let new_span_element = document.createElement("span");
+            new_span_element.className = "idea_text";
+
+            let timestamp_record;
+            if (localStorage.getItem('timestamp_record') == null) {
+                timestamp_record = [];
+            }
+            else {
+                timestamp_record = JSON.parse(localStorage.getItem('timestamp_record'));
+            }
+            for (let i = 0; i < timestamp_record.length; i++) {
+                if (index == i) {
+                    new_span_element.setAttribute("data-idea", timestamp_record[i]);
+                }
+            }
+
+            new_span_element.appendChild(document.createTextNode(idea));
+            li.appendChild(new_span_element);
+            items_list.appendChild(li);
+
+            let delete_button = document.createElement("button");
+            delete_button.className = "delete";
+            delete_button.appendChild(document.createTextNode("remove"));
+            li.appendChild(delete_button);
+
+            if (items_list.children.length == 0) {
+                items_list.classList.add("bg");
+            } else {
+                items_list.classList.remove("bg");
+            }
+        })
     }
     function clear_search_input() {
         if (search_icon.className == "fas fa-times") {
@@ -175,5 +303,7 @@ document.addEventListener("DOMContentLoaded", function () {
         year_long.innerHTML = `&nbsp20${year}`;
         time_long.innerHTML = `${hour} : ${minutes} : <span style="font-size: 1rem">${sec}</span>&nbsp <span style="font-size: 1.3rem">${anti_post_meridian}</span>`;
     }
+    update_ui_from_storage();
     setInterval(display_clock, 1000);
+    // localStorage.clear();
 });
