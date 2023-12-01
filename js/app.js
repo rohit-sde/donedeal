34a53f83cba015577cf11a46877185da072e89f8
Rohit Singh <rohitsinghpb4@gmail.com>
2023-12-02 04:17:40 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -8,82 +8,159 @@ document.addEventListener("DOMContentLoaded", function () {
     let year_long = document.querySelector(".year");
     let time_long = document.querySelector(".full_time");
 
-    // function add_item(e) {
-    //     e.preventDefault();
-    //     let new_value = document.getElementById("new").value;
-    //     let li = document.createElement("li");
-    //     li.className = "list_items";
-    //     li.appendChild(document.createTextNode(new_value));
-    //     items_list.appendChild(li);
-
-    //     let delete_button = document.createElement("button");
-    //     delete_button.className = "delete";
-    //     delete_button.appendChild(document.createTextNode("remove"));
-    //     li.appendChild(delete_button);
-    //     document.querySelector("#new").value = "";
-
-
-    //     if (items_list.children.length == 0) {
-    //         items_list.classList.add("bg");
-    //     } else {
-    //         items_list.classList.remove("bg");
-    //     }
-    // }
     function add_item(e) {
         e.preventDefault();
         let new_value = document.getElementById("new").value;
-        let li = document.createElement("li");
-        li.className = "list_items";
-        let new_span_element = document.createElement("span");
-        new_span_element.className = "idea_text";
+        let list_of_ideas = document.querySelectorAll(".list_items");
+        console.log(list_of_ideas.length);
+        if (list_of_ideas.length == 0) {
+            let li = document.createElement("li");
+            li.className = "list_items";
+            let new_span_element = document.createElement("span");
+            new_span_element.className = "idea_text";
 
-        let time_create = new Date();
-        let day_create = time_create.toLocaleString('default', { weekday: 'short' });
-        let date_create = time_create.toLocaleString('default', { day: '2-digit' });
-        let hour = time_create.getHours();
-        let minutes = time_create.getMinutes();
-        
-        let anti_post_meridian = "am";
+            let time_create = new Date();
+            let day_create = time_create.toLocaleString('default', { weekday: 'short' });
+            let date_create = time_create.toLocaleString('default', { day: '2-digit' });
+            let hour = time_create.getHours();
+            let minutes = time_create.getMinutes();
+            let sec = time_create.getSeconds();
+            let number_standard = time_create.getTime();
 
-        if (hour > 12 && hour < 24) {
-            hour = hour - 12;
-            anti_post_meridian = "pm";
+            let anti_post_meridian = "am";
+
+            if (hour > 12 && hour < 24) {
+                hour = hour - 12;
+                anti_post_meridian = "pm";
+            }
+            if (hour == 0) {
+                hour = 12 - hour;
+                anti_post_meridian = "am";
+            }
+            if (minutes < 10) {
+                minutes = `0${minutes}`;
+            }
+            if (sec < 10) {
+                sec = `0${sec}`;
+            }
+
+            new_span_element.setAttribute("data-idea", `Created on ${day_create} ${date_create} at ${hour}:${minutes} ${anti_post_meridian}`);
+            new_span_element.setAttribute("data-milisec", number_standard);
+            console.log(new_span_element.getAttribute("data-milisec"));
+
+            let timestamp_record;
+            if (localStorage.getItem('timestamp_record') == null) {
+                timestamp_record = [];
+            }
+            else {
+                timestamp_record = JSON.parse(localStorage.getItem('timestamp_record'));
+            }
+
+            // timestamp_record.push(new_span_element.getAttribute("data-idea"));
+            // timestamp_record.push(new_span_element.getAttribute("data-milisec"));
+
+            // let full_stamp = `${new_span_element.getAttribute("data-idea")}${new_span_element.getAttribute("data-milisec")}`;
+            // timestamp_record.push(new_span_element.getAttribute("data-idea"));
+            timestamp_record.push(new_span_element.getAttribute("data-milisec"));
+            // timestamp_record.push(full_stamp);
+            localStorage.setItem('timestamp_record', JSON.stringify(timestamp_record));
+
+            new_span_element.appendChild(document.createTextNode(new_value));
+            li.appendChild(new_span_element);
+            items_list.appendChild(li);
+
+            let delete_button = document.createElement("button");
+            delete_button.className = "delete";
+            delete_button.appendChild(document.createTextNode("remove"));
+            li.appendChild(delete_button);
+
+            storage(new_value);
+            document.querySelector("#new").value = "";
+
+            if (items_list.children.length == 0) {
+                items_list.classList.add("bg");
+            } else {
+                items_list.classList.remove("bg");
+            }
         }
-        if (hour == 0) {
-            hour = 12 - hour;
-            anti_post_meridian = "am";
+        for (let j = 0; j < list_of_ideas.length; j++) {
+            console.log(list_of_ideas.length);
+            if (list_of_ideas[j].firstChild.innerText != new_value) {
+                var fresh = true;
+            } else {
+                var fresh = false;
+                break;
+            }
         }
-        if (minutes < 10) {
-            minutes = `0${minutes}`;
+        if (fresh == false) {
+            alert("This item already exists.");
         }
-        new_span_element.setAttribute("data-idea", `Created on ${day_create} ${date_create} at ${hour}:${minutes} ${anti_post_meridian}`);
+        if (fresh == true) {
+            let li = document.createElement("li");
+            li.className = "list_items";
+            let new_span_element = document.createElement("span");
+            new_span_element.className = "idea_text";
 
-        let timestamp_record;
-        if (localStorage.getItem('timestamp_record') == null) {
-            timestamp_record = [];
-        }
-        else {
-            timestamp_record = JSON.parse(localStorage.getItem('timestamp_record'));
-        }
-        timestamp_record.push(new_span_element.getAttribute("data-idea"));
-        localStorage.setItem('timestamp_record', JSON.stringify(timestamp_record));
+            let time_create = new Date();
+            let day_create = time_create.toLocaleString('default', { weekday: 'short' });
+            let date_create = time_create.toLocaleString('default', { day: '2-digit' });
+            let hour = time_create.getHours();
+            let minutes = time_create.getMinutes();
+            let sec = time_create.getSeconds();
+            let number_standard = time_create.getTime();
 
-        new_span_element.appendChild(document.createTextNode(new_value));
-        li.appendChild(new_span_element);
-        items_list.appendChild(li);
+            let anti_post_meridian = "am";
 
-        let delete_button = document.createElement("button");
-        delete_button.className = "delete";
-        delete_button.appendChild(document.createTextNode("remove"));
-        li.appendChild(delete_button);
+            if (hour > 12 && hour < 24) {
+                hour = hour - 12;
+                anti_post_meridian = "pm";
+            }
+            if (hour == 0) {
+                hour = 12 - hour;
+                anti_post_meridian = "am";
+            }
+            if (minutes < 10) {
+                minutes = `0${minutes}`;
+            }
+            if (sec < 10) {
+                sec = `0${sec}`;
+            }
 
-        storage(new_value);
-        document.querySelector("#new").value = "";
+            new_span_element.setAttribute("data-idea", `Created on ${day_create} ${date_create} at ${hour}:${minutes} ${anti_post_meridian}`);
+            new_span_element.setAttribute("data-milisec", number_standard);
+            console.log(new_span_element.getAttribute("data-milisec"));
 
-        if (items_list.children.length == 0) {
-            items_list.classList.add("bg");
-        } else {
-            items_list.classList.remove("bg");
+            let timestamp_record;
+            if (localStorage.getItem('timestamp_record') == null) {
+                timestamp_record = [];
+            }
+            else {
+                timestamp_record = JSON.parse(localStorage.getItem('timestamp_record'));
+            }
+
+            // let full_stamp = `${new_span_element.getAttribute("data-idea")}${new_span_element.getAttribute("data-milisec")}`;
+            // timestamp_record.push(new_span_element.getAttribute("data-idea"));
+            timestamp_record.push(new_span_element.getAttribute("data-milisec"));
+            // timestamp_record.push(full_stamp);
+            localStorage.setItem('timestamp_record', JSON.stringify(timestamp_record));
+
+            new_span_element.appendChild(document.createTextNode(new_value));
+            li.appendChild(new_span_element);
+            items_list.appendChild(li);
+
+            let delete_button = document.createElement("button");
+            delete_button.className = "delete";
+            delete_button.appendChild(document.createTextNode("remove"));
+            li.appendChild(delete_button);
+
+            storage(new_value);
+            document.querySelector("#new").value = "";
+
+            if (items_list.children.length == 0) {
+                items_list.classList.add("bg");
+            } else {
+                items_list.classList.remove("bg");
+            }
         }
     }
     function remove_item(e) {
@@ -91,18 +168,19 @@ document.addEventListener("DOMContentLoaded", function () {
             if (confirm("Are you sure you want to delete this?")) {
                 let li = e.target.parentElement;
                 let li_content = li.firstChild.innerText;
-                let li_span_attr = li.firstChild.getAttribute("data-idea");
+                // let li_span_attr = li.firstChild.getAttribute("data-idea");
+                let li_span_attr = li.firstChild.getAttribute("data-milisec");
                 let ideas = JSON.parse(localStorage.getItem('ideas'));
                 let timestamp_record = JSON.parse(localStorage.getItem('timestamp_record'));
-                
+
                 ideas.forEach((idea, index) => {
-                    if(idea == li_content) {
+                    if (idea == li_content) {
                         ideas.splice(index, 1);
                     }
                     localStorage.setItem('ideas', JSON.stringify(ideas));
                 })
                 timestamp_record.forEach((timestamp, index) => {
-                    if(timestamp == li_span_attr) {
+                    if (timestamp == li_span_attr) {
                         timestamp_record.splice(index, 1);
                     }
                     localStorage.setItem('timestamp_record', JSON.stringify(timestamp_record));
@@ -191,7 +269,33 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             for (let i = 0; i < timestamp_record.length; i++) {
                 if (index == i) {
-                    new_span_element.setAttribute("data-idea", timestamp_record[i]);
+                    let time_from_epoch = new Date(Number(timestamp_record[i]));
+                    console.log(time_from_epoch);
+                    let day_create = time_from_epoch.toLocaleString('default', { weekday: 'short' });
+                    let date_create = time_from_epoch.toLocaleString('default', { day: '2-digit' });
+                    let hour = time_from_epoch.getHours();
+                    let minutes = time_from_epoch.getMinutes();
+                    let sec = time_from_epoch.getSeconds();
+                    // let number_standard = time_from_epoch.getTime();
+
+                    let anti_post_meridian = "am";
+
+                    if (hour > 12 && hour < 24) {
+                        hour = hour - 12;
+                        anti_post_meridian = "pm";
+                    }
+                    if (hour == 0) {
+                        hour = 12 - hour;
+                        anti_post_meridian = "am";
+                    }
+                    if (minutes < 10) {
+                        minutes = `0${minutes}`;
+                    }
+                    if (sec < 10) {
+                        sec = `0${sec}`;
+                    }
+                    new_span_element.setAttribute("data-idea", `Created on ${day_create} ${date_create} at ${hour}:${minutes} ${anti_post_meridian}`);
+                    new_span_element.setAttribute("data-milisec", timestamp_record[i]);
                 }
             }
 
